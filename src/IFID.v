538c6ca1e17e1a93920596ece998d7706adb171e
liu115 <jimmydiablo@gmail.com>
2016-12-09 19:50:29 +0800

    build IFID pipeline registers

--- src/IFID.v
+++ src/IFID.v
@@ -1,9 +1,9 @@
 module IFID (clk_i, start_i, addr_i, inst_i, addr_o, inst_o);
 
 input clk_i, start_i;
-input [4:0] addr_i, inst_i;
-output [4:0] addr_o, inst_o;
-reg [4:0] addr_o, inst_o;
+input [31:0] addr_i, inst_i;
+output [31:0] addr_o, inst_o;
+reg [31:0] addr_o, inst_o;
 
 always @ ( posedge clk_i or negedge start_i) begin
   if (~start_i) begin
