f876fa8fc42083426e2f7502917d7baedca8699d
liu115 <b03902105@linux11.csie.ntu.edu.tw>
2016-12-09 19:38:24 +0800

    Modified the regtest.v for test of pipeline registers

--- src/regtest.v
+++ src/regtest.v
@@ -2,8 +2,8 @@
 module reg_tb;
 reg Clk;
 reg Start;
-reg [4:0] a, b;
-wire [4:0] c, d;
+reg [4:0] a, b; //input
+wire [4:0] c, d; //output
 integer counter;
 always #(`CYCLE_TIME/2) Clk = ~Clk;
 
@@ -22,37 +22,24 @@ initial begin
 
   #(`CYCLE_TIME/4)
   Start = 1;
-  //$display("%dns %d %d %d %d", $stime, a, b, c, d);
-
-
-  //$display("%dns %d %d %d %d", $stime, a, b, c, d);
-
-  //$display("%dns %d %d %d %d", $stime, a, b, c, d);
-
-
-  //$display("%dns %d %d %d %d", $stime, a, b, c, d);
-  //$display("%dns %d %d %d %d", $stime, a, b, c, d);
 
 end
-
+always@(posedge Clk) begin
+  //change the input and output you want to show
+    a <= a + 1;
+    b <= b + 1;
+end
 always@(posedge Clk) begin
   if (counter == 5)
     $finish;
+  //change the input and output you want to show
   $display("%dns %d %d %d %d", $stime, a, b, c, d);
 
-  if (a == 5'b111) begin
-    a <= 5'b000;
-    b <= 5'b111;
-  end
-  else begin
-    a <= 5'b111;
-    b <= 5'b000;
-  end
+  
   counter = counter + 1;
-  //$display("%dns %d %d %d %d", $stime, a, b, c, d);
 
 end
-
+    //change the module you want
     IFID IFID(Clk, Start, a, b, c, d);
 
 endmodule
