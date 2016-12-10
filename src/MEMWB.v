de5398b161d66b5cac874c002f21dfe8b69b3949
liu115 <jimmydiablo@gmail.com>
2016-12-10 21:29:44 +0800

    Modified the port name of MEMWB.v.
    Change RDaddr into RegWaddr.

--- src/MEMWB.v
+++ src/MEMWB.v
@@ -6,24 +6,24 @@ module MEMWB
 	MemtoReg_i,
 	ReadData_i,
 	ALUdata_i,
-	RDaddr_i,
+    RegWaddr_i,
 	RegWrite_o,
 	MemtoReg_o,
 	ReadData_o,
 	ALUdata_o,
-	RDaddr_o
+	RegWaddr_o
 );
 
 input clk_i, start_i;
 input  RegWrite_i, MemtoReg_i;
 input [31:0] ReadData_i, ALUdata_i;
-input [4:0]  RDaddr_i;
+input [4:0]  RegWaddr_i;
 output  RegWrite_o, MemtoReg_o;
 output [31:0] ReadData_o, ALUdata_o;
-output [4:0] RDaddr_o;
+output [4:0] RegWaddr_o;
 reg  RegWrite_o, MemtoReg_o;
 reg [31:0] ReadData_o, ALUdata_o;
-reg [4:0] RDaddr_o;
+reg [4:0] RegWaddr_o;
 
 always @ ( posedge clk_i or negedge start_i) begin
   if (~start_i) begin
@@ -31,14 +31,14 @@ always @ ( posedge clk_i or negedge start_i) begin
     MemtoReg_o <= 0;
     ReadData_o <= 0;
     ALUdata_o <= 0;
-    RDaddr_o <=0;
+    RegWaddr_o <= 0;
   end
   else begin
     RegWrite_o <= RegWrite_i;
     MemtoReg_o <= MemtoReg_i;
     ReadData_o <= ReadData_i;
     ALUdata_o <= ALUdata_i;
-    RDaddr_o <= RDaddr_i;
+    RegWaddr_o <= RegWaddr_i;
   end
 end
 endmodule
